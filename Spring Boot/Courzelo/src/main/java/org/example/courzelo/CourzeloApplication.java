cd200496c43b615b4aef6be154ecad080f58f397
ramrym14 <125159717+ramrym14@users.noreply.github.com>
2024-06-21 16:02:32 +0100

    Your commit message

--- Spring Boot/Courzelo/src/main/java/org/example/courzelo/CourzeloApplication.java
+++ Spring Boot/Courzelo/src/main/java/org/example/courzelo/CourzeloApplication.java
@@ -2,8 +2,10 @@ package org.example.courzelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class CourzeloApplication {
 
 	public static void main(String[] args) {
---
+++ Spring Boot/Courzelo/src/main/java/org/example/courzelo/controllers/Project/ProjectController.java
@@ -0,0 +1,18 @@
+package org.example.courzelo.controllers.Project;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+
+@Tag(name = "project")
+public class ProjectController {
+
+
+
+}
