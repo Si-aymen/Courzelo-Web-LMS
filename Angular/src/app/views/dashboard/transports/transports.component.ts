a2824ff1577813ac92d314305ead7ef5d6edd860
aymen <aymen.rahali@esprit.tn>
2024-07-03 15:51:19 +0100

    add new dg to Transport comp

--- Angular/src/app/views/dashboard/transports/transports.component.ts
+++ Angular/src/app/views/dashboard/transports/transports.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/shared/services/product.service';
+import { TransportsService } from 'src/app/shared/services/transports.service';
 
 
 @Component({
@@ -10,19 +11,66 @@ import { ProductService } from 'src/app/shared/services/product.service';
 export class TransportsComponent implements OnInit{
 
   products$: any;
+  clientTransports$: any ; 
+  combinedList: any[] = [];
+
+  transports: any[] = [
+    { id: 1, Depature: 'Product 1', TimeOfDep: 10 },
+    { id: 2, Depature: 'Product 2', TimeOfDep: 20 }
+  ];
+
+
+  Lines$: any = [
+    { id: 1,Sector: 'Sud', Depature: 'Product 1', TimeOfDep: 10 },
+    { id: 2,Sector: 'Sud', Depature: 'Product 1', TimeOfDep: 30 },
+    { id: 3,Sector: 'Nord', Depature: 'Product 2', TimeOfDep: 20 }
+  ];
+
+  
+
   constructor(
-		private productService: ProductService
+		private productService: ProductService,
+		private transportsService: TransportsService
 	) { }
 
   ngOnInit() {
 
     this.products$ = this.productService.getProducts();
+    this.clientTransports$ = this.transportsService.getUsers();
+    //this.combineLists();
+    
 
   }
 
+  /*
+  combineLists() {
+    this.transports.forEach(transports => {
+      this.combinedList.push({
+        idT: transports.id,
+        typeT: 'product',
+        Depature: transports.Depature,
+        TimeOfDep: transports.TimeOfDep
+      });
+    });
 
+    this.clientTransports$.forEach(clientTransports => {
+      this.combinedList.push({
+        idC: clientTransports.id,
+        typeC: 'user',
+        avatar: clientTransports.avatar,
+        name: clientTransports.name
 
+        });
+    });
+  }
 
-
-
+  */s
 }
+
+
+
+
+
+
+
+
