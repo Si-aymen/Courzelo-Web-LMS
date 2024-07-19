34bde61ade3df89939175ed93fe6a1db813e8862
aymen <aymen.rahali@esprit.tn>
2024-07-19 19:08:11 +0100

    correcting errors

--- Spring Boot/Courzelo/src/main/java/org/example/courzelo/models/Transports.java
+++ Spring Boot/Courzelo/src/main/java/org/example/courzelo/models/Transports.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+
 import java.lang.annotation.Documented;
 import java.util.Date;
 
